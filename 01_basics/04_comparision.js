0f402b2d1cd854d0f083b020e7f2328bcc001657
priyarajmohnaty01 <119916872+jwjjhehsu@users.noreply.github.com>
2024-02-20 12:09:40 +0000

    comparision update

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,19 @@
+// console.log(2<-1);
+
+// console.log("2">1)
+// console.log("02">1)
+// console.log("3" >4)
+
+// console.log(null>=0)
+// console.log(null==0)
+// console.log(null>0)
+
+
+// console.log(undefined>=0)
+// console.log(undefined==0)
+// console.log(undefined>0)
+
+// Strict comparision
+// it checks the value as well as the type also
+console.log("2"== 2)//-> it gives true
+console.log("2"===2) //->it gives (false)
