bbebd640778b33840864c25d59448cd9983d8c34
priyarajmohnaty01 <119916872+jwjjhehsu@users.noreply.github.com>
2024-02-20 10:55:28 +0000

    Data types and variables

---
+++ 01_basics/01_Datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; //treat all JS CODE as newer version
+// alert(3+3) //because we are using the node js not the browser
+
+console.log(22+22);
+console.log("hello ");
+
+
+// Lets learn about the data types
+let name = "liku"
+let id = 22;
+let isLoggedin = false
+
+// PRIMITIVE DATA TYPES
+
+
+// number
+// bigint
+// string
+// boolean
+// null = standalone value
+// undefined = variable was declared but not given any value
+// symbol=>Uniqueness
+
+
+
+
+// OBJECT
+console.log(typeof 88);
+console.log(typeof null);  //->it will give the output of object
+console.log(typeof undefined);//->it will give the output as undefined
---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "123443"  
+accountCity = "jaipur"  //it is possible without declaring the variable
+let accountstate
+
+
+
+// accountId = 33 not allowed
+console.log(accountId)
+
+/* 
+prefer not to use "var" 
+because in issue in block scope or functional scope
+*/
+
+
+
+accountEmail = "liku@hjdbdj@gmial.com"
+accountPassword = "22j33"
+accountCity = "chenai"
+
+console.table([accountId, accountEmail, accountPassword, accountCity ,accountstate])
