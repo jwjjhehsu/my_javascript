5096dbc6c4bb419957c75130c1aaa13b458feb0d
priyarajmohnaty01 <119916872+jwjjhehsu@users.noreply.github.com>
2024-02-20 12:18:21 +0000

    datatype_more

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,25 @@
+//  Primitive
+// 
+/* These are all the 7 major primitive data types
+1) String 
+2) Number
+3) Boolean
+4) null
+5) undefined
+6) Symbol
+7) BigInt
+*/
+
+// Note---------]
+// Java-Script is a dynamic typed language it is not necessary to declare the variable types
+// ----------]
+
+// Reference Type or Non-Primittive Types
+/*
+1) Array
+2) Objects
+3) Functions
+*/
+
+
+// Continue
