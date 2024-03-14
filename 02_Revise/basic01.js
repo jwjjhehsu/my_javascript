854b004a912cb64fed40dcf26a956f96cb570090
priyarajmohnaty01 <119916872+jwjjhehsu@users.noreply.github.com>
2024-03-14 20:00:12 +0000

    Practce for again an effort

---
+++ 02_Revise/basic01.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+const accountId = 144553; //It cannot be further re-initialized
+let accoountEmail = "hitesh@gmail.com";
+var accountPassword = "12345";  //Not adviciable to use due to problem in block scope and functional scope
+accountCity = "jaipur"; //Also not adviceble to use it
+let accountState; //Undefined
+
+// Note-1
+// accountId = 2;
+// console.log(accountId);
+
+
+// Note-2
+// We can print the all the variables in a tabular form within a order manner
+console.table([accountId, accountPassword, accoountEmail, accountCity]);
+
+// Lets learn something about the javascript Data Types
+// ->
+"use strict"; //treat all JS code as newer version
+
+
+
+
